8c9f0ea2adfcfc116e4752949710d486610098ef
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-03-25 16:16:21 +0530

    fix suggestions given by the code reviewer

--- main.rb
+++ main.rb
@@ -14,8 +14,8 @@ module Enumerable
     return to_enum(:my_each_with_index) unless block_given?
 
     idx = 0
-    my_each do |elem|
-      yield(elem, idx)
+    my_each do |ele|
+      yield(ele, idx)
       idx += 1
     end
   end
@@ -35,64 +35,86 @@ module Enumerable
 
   # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
 
-  def my_all?(arg = nil, &prc)
-    return true if !block_given? && arg.nil? && include?(nil) == false && include?(false) == false
+    def my_all?(arg = nil)
+      return true if !block_given? && arg.nil? && include?(nil) == false && include?(false) == false
     return false unless block_given? || !arg.nil?
 
     if block_given?
-      my_each { |elem| return false if prc.call(elem) == false }
-    elsif arg.class == Regexp
-      my_each { |elem| return false if arg.match(elem).nil? }
-    elsif arg.class <= Numeric || arg.class <= String
-      my_each { |elem| return false if elem != arg }
+      my_each { |ele| return false unless yield(ele) }
+    elsif !arg.nil?
+      if arg.is_a?(Regexp)
+        my_each { |ele| return false unless arg != ele.to_s }
+      end
     else
-      my_each { |elem| return false if (elem.is_a? arg) == false }
+      my_each { |ele| return false unless ele }
     end
     true
   end
 
+  
   def my_any?(arg = nil, &prc)
-    return true if !block_given? && arg.nil? && my_each { |elem| return true if elem == true } && empty? == false
+    return true if !block_given? && arg.nil? && my_each { |ele| return true if ele == true } && empty? == false
     return false unless block_given? || !arg.nil?
 
-    my_each do |ele|
-      return true if prc.call(ele) == true
+    if block_given?
+      my_each { |ele| return true if prc.call(ele) }
+    elsif arg.class == Regexp
+      my_each { |ele| return true unless arg.match(ele).nil? }
+    elsif arg.class <= Numeric || arg.class <= String
+      my_each { |ele| return true if ele == arg }
+    else
+      my_each { |ele| return true if ele.class <= arg }
     end
     false
   end
 
-  def my_none?(arg = nil, &prc)
-    !my_any?(arg, &prc)
+  def my_none?(arg = nil)
+    !my_any?(arg)
   end
 
-  def my_count
+  def my_count(arg = nil, &prc)
     count = 0
-    my_each do |_|
-      count += 1
+    my_each do |ele|
+      if block_given?
+        count += 1 if prc.call(ele)
+      elsif !arg.nil?
+        count += 1 if ele == arg
+      else
+        count = length
+      end
     end
     count
-  end
-
-  def my_map(&prc)
+  end  
+  
+  def my_map(prc = nil)
     return enum_for(:map) unless block_given?
 
     mapped = []
-    my_each { |ele| mapped << prc.call(ele) }
+    my_each { |ele| mapped << prc.call(ele) } if block_given? && prc      
+    my_each { |ele| mapped << yield(ele) } if prc.nil?
     mapped
   end
 
-  def my_inject
-    acc = shift
-    item_removed = acc
-    my_each { |num| acc = yield(acc, num) }
-    unshift(item_removed)
-    acc
+  def my_inject(memo = nil, sym = nil, &prc)
+    memo = memo.to_sym if memo.is_a?(String) && !sym && !prc
+
+    if memo.is_a?(Symbol) && !sym
+      prc = memo.to_proc
+      memo = nil
+    end
+
+    sym = sym.to_sym if sym.is_a?(String)
+    prc = sym.to_proc if sym.is_a?(Symbol)
+
+    my_each { |ele| memo = memo.nil? ? ele : prc.yield(memo, ele) }
+    memo
   end
 end
+
 # rubocop:enable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
 
-def multiply_els
-  my_inject { |acc, curr| acc *= curr }
+def multiply_els(arr)
+  arr.my_inject { |acc, curr| acc *= curr }
 end
 
 puts multiply_els([2, 4, 5]) # => 40
