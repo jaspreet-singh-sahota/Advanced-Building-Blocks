9fa68df5a7038f104f3414d3817f190efadeed76
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-03-25 06:09:55 +0530

    fix suggesions given by the 1st code reviewer

--- main.rb
+++ main.rb
@@ -1,29 +1,28 @@
-module Enumerable
-  def my_each(&prc)
+module Enumerable 
+  def my_each
+    return to_enum(:my_each) unless block_given?
+
     i = 0
-    while i < length
-      if is_a?(Array)
-        yield self[i]
-      elsif prc.arity == 1
-        yield(assoc keys[i])
-      else
-        yield(keys[i], self[keys[i]])
-      end
+    size = self.size
+    while i < size
+      is_a?(Range) ? yield(min + i) : yield(self[i])
       i += 1
     end
-    self
   end
 
   def my_each_with_index
+    return to_enum(:my_each_with_index) unless block_given?
+
     idx = 0
-    while idx < length
-      yield(self[idx], idx)
+    my_each do |elem|
+      yield(elem, idx)
       idx += 1
     end
-    self
   end
 
   def my_select
+    return to_enum(:my_select) unless block_given?
+
     if is_a?(Array)
       selected = []
       my_each { |ele| selected << ele if yield ele }
@@ -34,25 +33,34 @@ module Enumerable
     selected
   end
 
-  def my_all?(&prc)
-    my_each do |ele|
-      return false if prc.call(ele) == false
+  def my_all?(arg = nil, &prc)
+    return true if !block_given? && arg.nil? && include?(nil) == false && include?(false) == false
+    return false unless block_given? || !arg.nil?
+
+    if block_given?
+      my_each { |elem| return false if prc.call(elem) == false }
+    elsif arg.class == Regexp
+      my_each { |elem| return false if arg.match(elem).nil? }
+    elsif arg.class <= Numeric || arg.class <= String
+      my_each { |elem| return false if elem != arg }
+    else
+      my_each { |elem| return false if (elem.is_a? arg) == false }
     end
     true
   end
 
-  def my_any?(&prc)
+  def my_any?(arg = nil, &prc)
+    return true if !block_given? && arg.nil? && my_each { |elem| return true if elem == true } && empty? == false
+    return false unless block_given? || !arg.nil?
+
     my_each do |ele|
       return true if prc.call(ele) == true
     end
     false
   end
 
-  def my_none?(&prc)
-    my_each do |ele|
-      return false if prc.call(ele) == true
-    end
-    true
+   def my_none?(arg = nil, &prc)
+    !my_any?(arg, &prc)
   end
 
   def my_count
@@ -64,20 +72,23 @@ module Enumerable
   end
 
   def my_map(&prc)
+    return enum_for(:map) unless block_given?
+    
     mapped = []
     my_each { |ele| mapped << prc.call(ele) }
     mapped
   end
 
   def my_inject
-    acc = shift # [1,2,3,4,5]
-    item_removed = acc # 1
+    acc = shift 
+    item_removed = acc 
     my_each { |num| acc = yield(acc, num) }
     unshift(item_removed)
     acc
   end
 
-  def multiply_els
-    my_inject { |acc, curr| acc *= curr }
-  end
+end
+
+def multiply_els
+  my_inject { |acc, curr| acc *= curr }
 end
